41e00a2c70a37df22955aab215bab80249249b99
Sonhaack <56017910+Sonhaack@users.noreply.github.com>
2022-05-09 16:45:30 +0200

    Build navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './request';
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey this is our app</h1>
-      <p>Sonny boy</p>
-      <Row title="ORIGINAL MOVIES" fetchUrl={requests.fetchTopRatedMovies} />
+    <div className="app">
+      
+      <Nav></Nav>
+      <Banner/>
+      <Row
+      title="TOP RATED MOVIES"
+      fetchUrl={requests.fetchTopRatedMovies}
+      IsLargeRow
+      />
       <Row title="TRENDING MOVIES" fetchUrl={requests.fetchTrendingMovies} />
+      <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies} />
+      <Row title="COMEDY MOVIES" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="ROMANCE MOVIES" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumantaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,15 @@
+import movieTrailer from 'movie-trailer';
 import React, {useEffect, useState} from 'react'
+import YouTube from 'react-youtube';
 import axios from './axios';
+import './Row.css';
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
-function Row({title, fetchUrl}) {
+function Row({title, fetchUrl,isLargeRow}) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl,setTrailerUrl] = useState("");
 
     // Hver gang dette køres, kommer der information, når koden kører
     useEffect(() => { 
@@ -17,21 +22,49 @@ function Row({title, fetchUrl}) {
         fecthData();
     }, [fetchUrl]);
 
-  // console.table(movies)
+  const opts = 
+  {
+      height: "390",
+      width: "100%",
+      playerVars:{
+          autoplay: 1,
+      },
+  };
+    const handleClick = (movie) =>
+    {
+        if(trailerUrl)
+        {
+            setTrailerUrl('');
+        }else
+        {
+            movieTrailer(movie?.name || "")
+            .then(url =>{
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get("v"));
+            })
+            .catch(error => console.log(error));
+        }
+    };
 
     return (
         <div className="row">
             <h2>{title}</h2>
 
             <div className="row_posters">
-                {/* posters  */}
-                
-                {movies.map(movie => (
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                {movies.map((movie) => (
+                    <img
+                    key={movie.id}
+                    onClick={() => handleClick(movie)}
+                    className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                    src={`${base_url}${
+                        isLargeRow ? movie.poster_path : movie.backdrop_path
+                    }`} 
+                    alt={movie.name}/>
                 ))}
                  </div>   
-        </div>
-    )
+                 {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}></YouTube>
+                 </div>
+    );
 }
 
-export default Row
+export default Row;
