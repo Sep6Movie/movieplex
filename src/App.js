475f1d54bb92b51483d743583556a1b9c863e005
AnneLindberg <aans@live.dk>
2022-05-10 17:30:09 +0200

    Removed files that did not work. Will create app from base again

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import './App.css';
 import Row from './Row';
-import requests from './request';
-import Banner from "./Banner";
-import Nav from "./Nav";
+import requests from './requests';
 
 function App() {
   return (
-    <div className="app">
-      
-      <Nav></Nav>
-      <Banner/>
-      <Row
-      title="TOP RATED MOVIES"
-      fetchUrl={requests.fetchTopRatedMovies}
-      IsLargeRow
-      />
-      <Row title="TRENDING MOVIES" fetchUrl={requests.fetchTrendingMovies} />
-      <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies} />
-      <Row title="COMEDY MOVIES" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="ROMANCE MOVIES" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumantaries} />
+    <div className="App">
+      <h1>MoviePlex</h1>
+      <Row title="TRENDING MOVIES" fetchUrl={requests.fetchTrending} />
+   <Row title="TOP RATED MOVIES" fetchUrl={requests.fetchTopRatedMovies}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,72 +1,21 @@
-import movieTrailer from 'movie-trailer';
-import React, {useEffect, useState} from 'react'
-import YouTube from 'react-youtube';
+import React, { useState, useEffect} from 'react'
 import axios from './axios';
-import './Row.css';
-import movieTrailer from 'movie-trailer';
 
-const base_url = "https://image.tmdb.org/t/p/original/"
-
-function Row({title, fetchUrl,isLargeRow})
-{
+function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
-    const [trailerUrl,setTrailerUrl] = useState("");
 
-    // Hver gang dette køres, kommer der information, når koden kører
     useEffect(() => { 
-        // if [] = null, run once when the row loads, and dont run again
         async function fecthData() {
             const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
+            console.log(request);
             return request;
         }
         fecthData();
-    }, [fetchUrl]);
+    }, []);
 
-  const opts = 
-  {
-      height: "390",
-      width: "100%",
-      playerVars:{
-          autoplay: 1,
-      },
-  };
-    const handleClick = (movie) =>
-    {
-        if(trailerUrl)
-        {
-            setTrailerUrl('');
-        }else
-        {
-            movieTrailer(movie?.name || "")
-            .then(url =>{
-                const urlParams = new URLSearchParams(new URL(url).search);
-                setTrailerUrl(urlParams.get("v"));
-            })
-            .catch((error) => console.log(error));
-        }
-    };
-
-    return (
-        <div className="row">
-            <h2>{title}</h2>
-
-            <div className="row_posters">
-                {movies.map((movie) => (
-                    <img
-                    key={movie.id}
-                    onClick={() => handleClick(movie)}
-                    className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-                    src={`${base_url}${
-                        isLargeRow ? movie.poster_path : movie.backdrop_path
-                    }`} 
-                    alt={movie.name}
-                    />
-                ))}
-                 </div>   
-                 {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
-                 </div>
-    );
+  return (
+    <h2>{title}</h2>
+  )
 }
 
-export default Row;
+export default Row
