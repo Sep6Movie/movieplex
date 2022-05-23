4226f38c3e21b96850c1344bbc713aed9693c830
AnneLindberg <aans@live.dk>
2022-05-23 10:31:52 +0200

    added upload user image

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -4,10 +4,11 @@ import { useAuth } from "../contexts/AuthContext"
 import { Link, useNavigate } from "react-router-dom"
 
 export default function Signup() {
+    const diaplayNameRef = useRef()
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
-    const { signup } = useAuth()
+    const { signup , currentUser } = useAuth()
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
@@ -27,7 +28,6 @@ export default function Signup() {
       } catch {
         setError("Failed to create an account")
       }
-  
       setLoading(false)
     }
   
@@ -37,10 +37,15 @@ export default function Signup() {
           <Card.Body>
             <h2 className="text-center mb-4">Sign Up</h2>
             {error && <Alert variant="danger">{error}</Alert>}
+            {JSON.stringify({currentUser})}
             <Form onSubmit={handleSubmit}>
               <Form.Group id="email">
                 <Form.Label>Email</Form.Label>
                 <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
+              <Form.Group id="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="displayName" ref={diaplayNameRef} required />
               </Form.Group>
               <Form.Group id="password">
                 <Form.Label>Password</Form.Label>
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react"
-import {Card, Button, Alert} from 'react-bootstrap'
-import { useAuth } from "../contexts/AuthContext"
+import React, { useState, useEffect } from "react"
+import {Card, Button, Alert, Image} from 'react-bootstrap'
+import { upload, useAuth } from "../contexts/AuthContext"
 import { Link, useNavigate } from "react-router-dom"
-
 
 export default function UserProfile() {
     const [error, setError] = useState("")
     const { currentUser, logout } = useAuth()
+    const [photo, setPhoto] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [photoURL, setPhotoURL] = useState("https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png");
     const navigate = useNavigate()
 
     async function handleLogout() {
@@ -20,19 +22,41 @@ export default function UserProfile() {
         }
       }
 
+      function handleChange(e) {
+        if (e.target.files[0]) {
+          setPhoto(e.target.files[0])
+        }
+      }
+
+      function handleClick() {
+       upload(photo, currentUser, setLoading);
+      }
+
+      useEffect(() => {
+        if (currentUser?.photoURL) {
+          setPhotoURL(currentUser.photoURL);
+        }
+      }, [currentUser])
+    
+
   return (
     <>
-    <Card>
-      <Card.Body>
+
         <h2 className="text-center mb-4">Profile</h2>
+        <Card>
+      <Card.Body>
         {error && <Alert variant="danger">{error}</Alert>}
         <strong>Email:</strong> {currentUser.email}
-        {JSON.stringify({currentUser})}
-        <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-          Update Profile
-        </Link>
-      </Card.Body>
+        <div className="fields">
+      <input type="file" onChange={handleChange} />
+      <button disabled={loading || !photo} onClick={handleClick}>Upload</button>
+     <img src={photoURL} alt="Avatar" className="img-fluid rounded-circle img-thumbnail rounded float-right" />
+     </div>
+    </Card.Body>
     </Card>
+        {/* <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+          Update Profile
+        </Link> */}
     <div className="w-100 text-center mt-2">
       <Button variant="link" onClick={handleLogout}>
         Log Out
