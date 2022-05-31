dbea376c5e1c8d96f8ffc0eeba3c2c0974c0e333
AnneLindberg <aans@live.dk>
2022-05-31 13:19:48 +0200

    test

--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
 import { Link, useNavigate } from "react-router-dom"
 import { useAuth } from "../../contexts/AuthContext"
-import "./Authentication.css"
+import "./Authentication.css" 
 
 export default function Login() {
   const emailRef = useRef()
